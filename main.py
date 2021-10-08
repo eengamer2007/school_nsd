768d07c6fccc849540bd37930d2f0818d3f6f3f9
eengamer2007 <61841788+eengamer2007@users.noreply.github.com>
2021-10-08 14:50:03 +0200

    lots happened and me lazy

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from tkinter import font
 import nsd_parser
 import sys
 
-#get input file name
+#get input file name, handeling if file exist happens in nsd_parser.py
 try:
     input_file = sys.argv[1]
     print("input file: " + input_file)
 except IndexError:
-    #default if no file is given
+    #default to nsd.txt if no file is given
     print("no input file given\nusing default: nsd.txt")
     input_file = "nsd.txt"
 
-# imporant variables
+# global variables
 TEXT_OFFSET = 5
 BLOCK_OFFSET = 2
 BLOCK_HEIGHT = 50
@@ -49,8 +49,7 @@ def rect(width, pos, indent, indent_back = 0, indent_front = 0):
         BLOCK_OFFSET + indent * INDENT_SIZE + indent_front, pos * BLOCK_HEIGHT + BLOCK_OFFSET,
         width + BLOCK_OFFSET - indent_back, pos * BLOCK_HEIGHT + BLOCK_OFFSET,
         width + BLOCK_OFFSET - indent_back, pos * BLOCK_HEIGHT + BLOCK_HEIGHT + BLOCK_OFFSET,
-        BLOCK_OFFSET + indent * INDENT_SIZE + indent_front,
-        pos * BLOCK_HEIGHT + BLOCK_HEIGHT + BLOCK_OFFSET,
+        BLOCK_OFFSET + indent * INDENT_SIZE + indent_front, pos * BLOCK_HEIGHT + BLOCK_HEIGHT + BLOCK_OFFSET,
         fill="white", outline="black"
     )
 
@@ -63,8 +62,8 @@ def get_array_len(array) -> int:
                 array_len += get_array_len(i[1:]) + 1
             elif i[0] == "if":
                 array_len += 1
-                array_len_1 = get_array_len(i[1])
-                array_len_2 = get_array_len(i[2])
+                array_len_1 = get_array_len(i[2])
+                array_len_2 = get_array_len(i[3])
                 if array_len_1 > array_len_2:
                     array_len += array_len_1
                 else:
@@ -105,25 +104,44 @@ def if_pass(statment, if_arr, else_arr, indent, indent_back = 0):
     location += 1
     for i in range(length):
         rect(BLOCK_WIDTH, location, indent, indent_back=indent_back, indent_front=usewidth)
+        try:
+            canvas.create_text(
+                BLOCK_OFFSET + indent * INDENT_SIZE + TEXT_OFFSET,
+                BLOCK_OFFSET + BLOCK_HEIGHT * location + TEXT_OFFSET,
+                text= if_arr[i], font = font.Font(size = -(BLOCK_HEIGHT - (TEXT_OFFSET * 2))),
+                anchor=tk.NW
+            )
+        except IndexError:
+            pass
         rect(BLOCK_WIDTH, location, indent, indent_back=indent_back+usewidth)
+        try:
+            canvas.create_text(
+                BLOCK_OFFSET + indent * INDENT_SIZE + TEXT_OFFSET + usewidth,
+                BLOCK_OFFSET + BLOCK_HEIGHT * location + TEXT_OFFSET,
+                text= else_arr[i], font = font.Font(size = -(BLOCK_HEIGHT - (TEXT_OFFSET * 2))),
+                anchor=tk.NW
+            )
+        except IndexError:
+            pass
         location += 1
     
 #drawing the blocks 
-def standart_pass(array, indent, indent_back = 0):
+def standart_pass(array, indent, indent_back = 0, last = None):
     global location
     array_len = get_array_len(array)
     canvas.create_line(
         BLOCK_OFFSET + indent * INDENT_SIZE,
         BLOCK_OFFSET + location * BLOCK_HEIGHT,
         BLOCK_OFFSET + indent * INDENT_SIZE,
-        BLOCK_OFFSET + BLOCK_HEIGHT * (array_len + location),
+        BLOCK_OFFSET + BLOCK_HEIGHT * (array_len + location)
     )
-    canvas.create_line(
-        BLOCK_OFFSET + indent * INDENT_SIZE,
-        BLOCK_OFFSET + BLOCK_HEIGHT * (array_len + location),
-        BLOCK_OFFSET + (indent - 1) * INDENT_SIZE,
-        BLOCK_OFFSET + BLOCK_HEIGHT * (array_len + location),
-    )
+    if last != "for":
+        canvas.create_line(
+            BLOCK_OFFSET + indent * INDENT_SIZE,
+            BLOCK_OFFSET + BLOCK_HEIGHT * (array_len + location),
+            BLOCK_OFFSET + (indent - 1) * INDENT_SIZE,
+            BLOCK_OFFSET + BLOCK_HEIGHT * (array_len + location)
+        )
     for i in array:
         if isinstance(i, list):
             match i[0]:
@@ -146,17 +164,30 @@ def standart_pass(array, indent, indent_back = 0):
                     canvas.create_line(
                         BLOCK_OFFSET + INDENT_SIZE * (indent + 1),
                         BLOCK_OFFSET + (location + 1) * BLOCK_HEIGHT,
-                        BLOCK_OFFSET + BLOCK_WIDTH - (indent - 1) * INDENT_SIZE,
-                        BLOCK_OFFSET + BLOCK_HEIGHT * (location + 1),
+                        BLOCK_OFFSET + BLOCK_WIDTH - (indent) * INDENT_SIZE,
+                        BLOCK_OFFSET + BLOCK_HEIGHT * (location + 1)
+                    )
+                    canvas.create_line(
+                        BLOCK_OFFSET + INDENT_SIZE * (indent),
+                        BLOCK_OFFSET + (location + get_array_len(i)) * BLOCK_HEIGHT,
+                        BLOCK_OFFSET + BLOCK_WIDTH - (indent) * INDENT_SIZE,
+                        BLOCK_OFFSET + BLOCK_HEIGHT * (location + get_array_len(i))
                     )
                     location += 1
-                    standart_pass(i[2:], indent + 1)
+                    standart_pass(i[2:], indent + 1, last="for")
+                    location += 1
                 case "if":
-                    if_standart_pass(i[1], i[2], i[3], indent)
+                    if_pass(i[1], i[2], i[3], indent)
                 case _:
                     raise(KeyboardInterrupt)
         else:
             rect(BLOCK_WIDTH,location,indent, indent_back=indent_back)
+            canvas.create_text(
+                BLOCK_OFFSET + indent * INDENT_SIZE + TEXT_OFFSET,
+                BLOCK_OFFSET + BLOCK_HEIGHT * location + TEXT_OFFSET,
+                text= i, font = font.Font(size = -(BLOCK_HEIGHT - (TEXT_OFFSET * 2))),
+                anchor=tk.NW
+            )
             location += 1
 
 #start the block drawing
